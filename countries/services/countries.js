2ce21e8b845e766071f28fbf7495a8bfa6f01a6b
Vladimir <dtwdevelop@gmail.com>
2024-02-07 10:10:07 +0000

    filter and env

--- countries/services/countries.js
+++ countries/services/countries.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 const baseUrl ="https://studies.cs.helsinki.fi/restcountries/api/all"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather"
+const token = import.meta.env.VITE_SOME_KEY
 
 const getCountries = ()=>{
     const request = axios.get(baseUrl)
-    return request.then(res=>res.data)
+    return request.then(res=>res.data).catch(err=>console("api failed load"))
+} 
+const getWeather = (city)=>{
+    const request = axios.get(`${apiUrl}?q=${city}&appid=${token}`)
+    return request.then(res=>res.data).catch(err=>console("api failed load"))
 } 
 
-export default{ getCountries}
+export default{ getCountries ,getWeather}
--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,84 +1,129 @@
-import { useState ,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Countries from '/services/countries'
 
-const List = ({countries}) => {
-  if(countries.length > 10){
+const Weather = ({ item }) => {
+  const [flag, setVal] = useState(false)
+  const [data, setData] = useState({})
+
+  const loadWeatherApi = () => {
+    Countries.getWeather(item.capital).then(res => {
+      setData({ temp: res.main.temp, wind: res.wind.speed, img: `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png` })
+
+
+    })
+
+  }
+
+
+  useEffect(loadWeatherApi, [])
+
+  return (
+    <div>
+      <h4>Weather in {item.capital}</h4>
+
+      <p>temperature {(data.temp - 273).toFixed()}</p>
+      <img src={data.img} />
+      <p>wind {data.wind} m/s</p>
+    </div>
+  )
+}
+
+const List = ({ countries, onView }) => {
+
+  const onViewDetails = (country) => {
+    onView(country)
+  }
+
+  if (countries.length > 10) {
     return (
       <div>
         <p>Too many matches specify another filter</p>
       </div>
     )
   }
-  else if(countries.length == 1){
-   
+  else if (countries.length == 1) {
+
     return (
       <div>
         <ul>
-        {countries.map(item =>
-        <li key={item.capital}>
-         <h3> {item.country} </h3> 
-         <p>capital {item.capital} </p>
-         <p>area {item.area} </p>
-        <p>languages</p>
-        <p>{item.languages.ara}</p>
-        <img src={item.flag} width="200" />
-        <h4>Weather in {item.capital}</h4>
-          </li>
-        )}
+          {countries.map(item =>
+            <li key={item.capital}>
+              <h3> {item.country} </h3>
+              <p>capital {item.capital} </p>
+              <p>area {item.area} </p>
+              <p>languages</p>
+              <ul>
+                {Object.entries(item.languages).map(([k, v]) => (<li key={k}>{v}</li>))}
+              </ul>
+
+              <img src={item.flag} width="200" />
+              <Weather item={item} />
+
+            </li>
+          )}
         </ul>
       </div>
     )
   }
 
   else {
-  return (
+    return (
       <div>
-     
-     <ul>
-        {countries.map(country =>
-        <li key={country.capital}>{country.country}  <button type='button' >show</button> </li>
-        )}
+
+        <ul>
+          {countries.map(country =>
+            <li key={country.capital}>{country.country}  <button type='button' onClick={() => onViewDetails(country.country)} >show</button> </li>
+          )}
         </ul>
       </div>
     )
-        }
   }
+}
 
 function App() {
-  
-  const [countries ,setContries] = useState([])
-  const [val ,setvalue] = useState()
+
+  const [countries, setContries] = useState([])
+  const [val, setvalue] = useState()
 
 
-  const loadFromApi = (val) =>{
-    Countries.getCountries().then(res=>{
-      
-     const items = res.map(item=>{
-      return {
-        country : item.name.common,
-        capital : item.capital,
-        area : item.area,
-        languages : item.languages,
-        flag : item.flags.png
-      }
+  const loadFromApi = (val) => {
+    Countries.getCountries().then(res => {
+
+      const items = res.map(item => {
+        return {
+          country: item.name.common,
+          capital: item.capital,
+          area: item.area,
+          languages: item.languages,
+          flag: item.flags.png,
+          latlng: item.latlng
+        }
       })
-      const allCountries =  items.filter(item=> item.country.includes(val))
-     
+      const allCountries = items.filter(item => item.country.toLowerCase().includes(val.toLowerCase()))
+
       setContries(allCountries)
-      //console.log(allCountries)
+
     })
   }
   //useEffect(loadFromApi,[])
 
-  const SearchCountry = (event)=>{
+  const SearchCountry = (event) => {
     loadFromApi(event.target.value)
-   }
+  }
+
+  const changeView = (country) => {
+    const res = countries.filter((c) => c.country == country)
+    setContries(res)
+  }
 
   return (
     <>
       <div>
+        <form>
         <input value={val} onChange={SearchCountry} />
-        <List countries={countries}/>
+        </form>
+      
+        <List countries={countries} onView={changeView} />
       </div>
     </>
   )
